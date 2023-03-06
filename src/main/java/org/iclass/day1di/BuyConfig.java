8ea58b556c20441bd3865be00e9bc45f1b52424c
kimsewhee <koreait.sec2020@gmail.com>
2023-03-06 15:10:41 +0900

    _#5 주석 수정

--- src/main/java/org/iclass/day1di/BuyConfig.java
+++ src/main/java/org/iclass/day1di/BuyConfig.java
@@ -12,7 +12,7 @@ public class BuyConfig {
 //@ComponentScan(basePackages = "org.iclass.day1di") : basePackages 로 지정된 패키지의 모든 클래스를 스캔하겠다.
 //				@Component 를 찾아서 bean 을 만들기 위한 스캔입니다. 배열형식으로 여러 패키지를 설정할수 있습니다.
 //				bean (객체) 을 만들어서 스프링 컨테이너에 저장하고 관리합니다.
-//주의 : @ComponentScans 는 @ComponentScan의 배열
+//주의 : @ComponentScans 는 @ComponentScan의 배열. 
 //@Configuration
 //@ComponentScans({ 
 //    @ComponentScan(basePackages = "com.baeldung.annotations"), 
