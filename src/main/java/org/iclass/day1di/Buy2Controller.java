6e1d74b38d7e1480b3cc08bbfdb0d3ef3a355549
kimsewhee <koreait.sec2020@gmail.com>
2023-03-06 14:48:07 +0900

    _#3 bean 생성정의와 설정

---
+++ src/main/java/org/iclass/day1di/Buy2Controller.java
@@ -0,0 +1,32 @@
+package org.iclass.day1di;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import lombok.Setter;
+
+@Component
+public class Buy2Controller {
+	
+	@Autowired				//이미 만들진 Buy2Service 객체(bean)을 자동 주입
+	private Buy2Service service;
+	
+	/*
+	 * public Buy2Controller(Buy2Service service) {
+	 * System.out.println("::: Buy2Controller 커스텀 생성자 :::"); this.service = service;
+	 * }
+	 */
+	public Buy2Controller() {
+		System.out.println("::: Buy2Controller 기본 생성자 :::");
+	}
+	
+	public void setService(Buy2Service service) {
+		System.out.println("--- Buy2Controller 서비스 setter --- ");
+		this.service = service;
+	}
+	
+	public void buy() {
+		System.out.println("--- Buy2Controller buy() 메소드 ---");
+		service.buy();
+	}
+}
+//@Component 어노테이션 : bean으로 만들어질 클래스 위에 설정합니다.
---
+++ src/main/java/org/iclass/day1di/Buy2Service.java
@@ -0,0 +1,33 @@
+package org.iclass.day1di;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import lombok.Setter;
+
+@Component
+public class Buy2Service {
+	
+	@Autowired
+	private Buy2Dao dao;
+	
+//	public Buy2Service(Buy2Dao dao) {
+//		System.out.println("::: Buy2Service 커스텀 생성자 :::");
+//		this.dao = dao;
+//	}
+	
+	public Buy2Service() {
+		System.out.println("::: Buy2Service 기본 생성자 :::");
+	}
+	
+	public void setDao(Buy2Dao dao) {
+		System.out.println("::: Buy2Service 다오 setter :::");
+		this.dao = dao;
+	}
+	
+	public void buy() {
+		System.out.println("--- Buy2Service buy() 메소드 ---");
+		dao.buy();
+	}
+
+}
